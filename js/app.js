94df94f76f3d5fe6e819459ce2cc474a8d3326cc
Michelle Willcox <shellwillcox@gmail.com>
2015-02-17 22:30:28 -0500

    Ui router functionality implemented

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-var pickupApp = angular.module('pickupApp', ["firebase"]);
+var pickupApp = angular.module('pickupApp', [
+	'firebase',
+	'ngRoute'
+	]);
 
-pickupApp.controller('PickupCtrl', function($scope, $firebase){
+pickupApp.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider.
+			when('/home',
+			{
+				templateUrl: 'home.html'
+			}).
+			when('/setup', {
+				templateUrl: 'setup.html',
+				controller: 'SetupCtrl'
+			}).
+			when('/games', {
+				templateUrl: 'games.html',
+				controller: 'GamesCtrl'
+			}).
+			otherwise({
+				redirectTo: '/home.html'
+			});
+	}]);
+
+pickupApp.controller('SetupCtrl', function($scope, $firebase){
 	var ref = new Firebase('https://popping-inferno-9074.firebaseio.com/');
 	$scope.players = $firebase(ref).$asArray();
   
@@ -8,6 +31,11 @@ pickupApp.controller('PickupCtrl', function($scope, $firebase){
     	$scope.players.$add({name: $scope.name});
     	$scope.name = " ";
     };
+});
+
+pickupApp.controller('GamesCtrl', function($scope, $firebase){
+	var ref = new Firebase('https://popping-inferno-9074.firebaseio.com/');
+	$scope.players = $firebase(ref).$asArray();
 });
 
 
