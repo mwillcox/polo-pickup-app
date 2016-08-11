d92d4dd543bffc4a355bc63c50bc50c421255770
Michelle Willcox <shellwillcox@gmail.com>
2016-08-11 00:27:11 -0700

    create weather directive

--- js/app.js
+++ js/app.js
@@ -96,5 +96,27 @@ pickupApp.controller("GamesCtrl", ["$scope", "players", "games", "teams",
 	}
 ]);
 
+pickupApp.directive('weatherIcon', function() {
+    return {
+        restrict: 'E', replace: true,
+        scope: {
+            cloudiness: '@'
+        },
+        controller: function($scope) {
+            $scope.imgurl = function() {
+                var baseUrl = 'https://ssl.gstatic.com/onebox/weather/128/';
+                if ($scope.cloudiness < 20) {
+                    return baseUrl + 'sunny.png';
+                } else if ($scope.cloudiness < 90) {
+                   return baseUrl + 'partly_cloudy.png';
+                } else {
+                    return baseUrl + 'cloudy.png';
+                }
+            };
+        },
+        template: '<div style="float:left"><img ng-src="{{ imgurl() }}"></div>'
+    };
+});
+
 
 
